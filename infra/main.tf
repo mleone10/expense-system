46cc92d29935647f5bcafea92962007a20421760
mleone10 <mleone10@gmail.com>
2021-10-21 00:46:21 -0400

    Initial cognito configuration

--- infra/main.tf
+++ infra/main.tf
@@ -9,3 +9,36 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_cognito_user_pool" "users" {
+  name = "${var.project_name}-users"
+}
+
+resource "aws_cognito_identity_provider" "google" {
+  user_pool_id  = aws_cognito_user_pool.users.id
+  provider_name = "Google"
+  provider_type = "Google"
+
+  provider_details = {
+    authorize_scopes              = "profile email openid"
+    token_url                     = "https://www.googleapis.com/oauth2/v4/token"
+    token_request_method          = "POST"
+    oidc_issuer                   = "https://accounts.google.com"
+    authorize_url                 = "https://accounts.google.com/o/oauth2/v2/auth"
+    attributes_url                = "https://people.googleapis.com/v1/people/me?personFields="
+    attributes_url_add_attributes = "true"
+    client_id                     = "${vars.google_oauth_client_id}"
+    client_secret                 = "${vars.google_oauth_client_secret}"
+  }
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name                                 = "${var.project_name}-client"
+  user_pool_id                         = aws_cognito_user_pool.users.id
+  supported_identity_providers         = ["Google"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["email", "openid", "profile"]
+  callback_urls                        = ["https://example.com/callback"]
+  generate_secret                      = true
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,15 @@ variable "project_name" {
   type    = string
   default = "expense-system"
 }
+
+variable "google_oauth_client_id" {
+  type      = string
+  sensitive = true
+  default   = "fakeId"
+}
+
+variable "google_oauth_client_secret" {
+  type      = string
+  sensitive = true
+  default   = "fakeSecret"
+}
