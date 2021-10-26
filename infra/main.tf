3b0b11d0d9456d39974eee490d390a88ed4d9b93
mleone10 <mleone10@gmail.com>
2021-10-25 22:20:14 -0400

    Instantiate CloudFront CDN

--- infra/main.tf
+++ infra/main.tf
@@ -60,4 +60,43 @@ resource "aws_cognito_user_pool_client" "client" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "leone-${var.project_name}"
+  acl    = "private"
+}
+
+locals {
+  s3_origin_id = "${vars.project_name}-origin"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  price_class         = "PriceClass_100"
+
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET"]
+    cached_methods         = ["GET"]
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = false
+      }
+    }
+  }
+
+  restrictions {
+    restriction_type = "none"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
