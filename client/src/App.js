a4bb45b346f5335f18ab8d20dbb16b7ebde0bab5
Mario Leone <mleone10@gmail.com>
2021-11-27 00:51:07 -0500

    Add page title

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,24 @@ function App() {
 
 function AppHeader() {
   return (
-    <header className="app-header">
-      <LoginButton />
+    <header className="header">
+      <div className="header-block">
+        <h1 className="header-title">Expense System</h1>
+        <p className="header-subtitle">
+          Reimbursement simplified.
+        </p>
+      </div>
+      <div className="header-block">
+        <SignInButton />
+      </div>
     </header>
   )
 }
 
-function LoginButton() {
+function SignInButton() {
   return (
-    <a href="https://auth.expense.mleone.dev/login?client_id=6ka3m790cv5hrhjdqt2ju89v45&response_type=code&scope=email+openid+profile&redirect_uri=https://expense.mleone.dev" className="login-button">
-      Login
+    <a href="https://auth.expense.mleone.dev/login?client_id=6ka3m790cv5hrhjdqt2ju89v45&response_type=code&scope=email+openid+profile&redirect_uri=https://expense.mleone.dev" className="header-button">
+      Sign In
     </a>
   )
 }
