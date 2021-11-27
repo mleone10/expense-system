2e4a6ba2b603e0022dfcf9601e2fc5c89cc45f97
Mario Leone <mleone10@gmail.com>
2021-11-26 21:34:58 -0500

    Add login button linking to Cognito signin page

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <AppHeader />
     </div>
   );
+}
+
+function AppHeader() {
+  return (
+    <header className="app-header">
+      <LoginButton />
+    </header>
+  )
+}
+
+function LoginButton() {
+  return (
+    <a href="https://auth.expense.mleone.dev/login?client_id=6ka3m790cv5hrhjdqt2ju89v45&response_type=code&scope=email+openid+profile&redirect_uri=https://expense.mleone.dev" className="login-button">
+      Login
+    </a>
+  )
 }
 
 export default App;
