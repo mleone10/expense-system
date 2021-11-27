1b5eca79144887148670cd02c1f30d85d86c9773
Mario Leone <mleone10@gmail.com>
2021-11-27 14:23:13 -0500

    First attempt at API gateway configuration

--- infra/main.tf
+++ infra/main.tf
@@ -224,3 +224,36 @@ resource "aws_lambda_function" "lambda" {
   handler       = "bin/lambdaserver"
   runtime       = "go1.x"
 }
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "${var.project_name}-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "api_stage_prod" {
+  name        = "prod"
+  api_id      = aws_apigatewayv2_api.api.id
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "api_integration" {
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_uri    = aws_lambda_function.lambda.invoke_arn
+  integration_type   = "AWS PROXY"
+  integration_method = "ANY"
+}
+
+resource "aws_apigatewayv2_route" "api_route_all" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.api_integraion.id}"
+}
+
+resource "aws_lambda_permission" "api_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
