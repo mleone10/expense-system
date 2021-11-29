347bf08d96812c812288c7bdca2b66677a572820
mleone10 <mleone10@gmail.com>
2021-11-29 13:10:14 -0500

    Link to Twitter bio in footer

--- client/src/App.js
+++ client/src/App.js
@@ -46,7 +46,7 @@ function AppContent() {
 function AppFooter() {
   return (
     <div className="app-footer">
-      <p>Copyright &copy; 2021 Mario Leone</p>
+      <p>Copyright &copy; 2021 <a href="https://twitter.com/mleone5244">Mario Leone</a></p>
       <p>Money icon by <a href="https://icons8.com">Icons8</a></p>
     </div>
   )
