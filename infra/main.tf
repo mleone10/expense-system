b2cc8714af218f9bafd4332eab76487c2bd15634
Mario Leone <mleone10@gmail.com>
2021-11-29 00:35:13 -0500

    Redirect http 403s from cloudfront

--- infra/main.tf
+++ infra/main.tf
@@ -163,7 +163,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   custom_error_response {
-    error_code         = 404
+    error_code         = 403
     response_code      = 200
     response_page_path = "/"
   }
