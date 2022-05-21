6a2440a650747c0d94d2a2a2f1a627f1d96c49bc
Mario Leone <mleone10@gmail.com>
2022-05-21 10:30:30 -0400

    Replace sign-in button styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -46,7 +46,7 @@ function AppFooter() {
 function SignInButton() {
   const signInLink = `https://auth.expense.mleone.dev/login?client_id=6ka3m790cv5hrhjdqt2ju89v45&response_type=code&scope=email+openid+profile&redirect_uri=${process.env.NODE_ENV === "development" ? 'http://localhost:3000' : 'https://expense.mleone.dev'}/auth/callback`
   return (
-    <a href={signInLink} className="header-button">
+    <a href={signInLink} className="sign-in-button">
       Sign In
     </a>
   )
