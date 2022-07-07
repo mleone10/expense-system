18c7ad570136765f5e480fe36b049598377c1818
Mario Leone <mleone10@gmail.com>
2022-07-07 18:18:06 -0400

    Split Home to New Directory (#10)
    * Move Home view to its own file
    
    * Update footer for multiple icons

--- client/src/components/AppFooter/AppFooter.tsx
+++ client/src/components/AppFooter/AppFooter.tsx
@@ -4,7 +4,7 @@ function AppFooter() {
   return (
     <footer>
       <p>&copy; <a href="https://twitter.com/mleone5244">Mario Leone</a></p>
-      <p>Money icon by <a href="https://icons8.com">Icons8</a></p>
+      <p>Icons by <a href="https://icons8.com">Icons8</a></p>
     </footer>
   )
 }
