9aed19ccac50779e6b06cf8b98a4e3796c07c8ff
Mario Leone <mleone10@gmail.com>
2022-08-30 22:47:11 -0400

    Remove refactored endpoints from legacy api.go

--- server/adapters/rest/api.go
+++ server/adapters/rest/api.go
@@ -7,14 +7,9 @@ package rest
 // 	s.router.Route("/api", func(r chi.Router) {
 
 // 		r.Group(func(r chi.Router) {
-// 			r.Route("/orgs", func(r chi.Router) {
 // 				r.Post("/", s.handleCreateNewOrg())
 // 				r.Route(fmt.Sprintf("/{%s}", urlParamOrgId), func(r chi.Router) {
 // 				})
-// 			})
-
-// 			r.Route("/user", func(r chi.Router) {
-// 				r.Get("/", s.handleGetUser())
 // 			})
 // 		})
 // 	})
