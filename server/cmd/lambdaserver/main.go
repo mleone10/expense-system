f8086596bffa1c49253d1012374d983dfb3a7e72
Mario Leone <mleone10@gmail.com>
2022-10-28 01:15:20 -0400

    Impl. lambda handler configuration

--- server/cmd/lambdaserver/main.go
+++ server/cmd/lambdaserver/main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	"github.com/mleone10/expense-system/adapters/cognito"
+	"github.com/mleone10/expense-system/adapters/dynamodb"
+	"github.com/mleone10/expense-system/adapters/rest"
+	"github.com/mleone10/expense-system/adapters/stdlogger"
+	"github.com/mleone10/expense-system/service"
 )
 
 func main() {
-	// server, err := api.NewServer(api.Config{
-	// 	CognitoClientId:     "6ka3m790cv5hrhjdqt2ju89v45",
-	// 	CognitoClientSecret: os.Getenv("COGNITO_CLIENT_SECRET"),
-	// 	ClientHostname:      "expense.mleone.dev",
-	// 	ClientScheme:        "https",
-	// })
-	// if err != nil {
-	// 	log.Fatalf("Failed to initialize server: %v", err)
-	// }
+	authClient := cognito.NewAuthClient(
+		cognito.WithClientHostname("expense.mleone.dev"),
+		cognito.WithClientScheme("https"),
+		cognito.WithCognitoClientId("6ka3m790cv5hrhjdqt2ju89v45"),
+		cognito.WithCognitoClientSecret(os.Getenv("COGNITO_CLIENT_SECRET")),
+	)
 
-	// adapter := httpadapter.New(server)
+	orgRepo, _ := dynamodb.NewClient()
 
-	// lambda.Start(serverHandler(adapter))
+	orgService := service.NewOrgService(orgRepo)
+
+	authenticatedUserService := service.NewAuthenticatedUserService(authClient)
+
+	logger := stdlogger.NewLogger()
+
+	server, _ := rest.NewServer(
+		rest.WithAuthClient(authClient),
+		rest.WithOrgService(orgService),
+		rest.WithAuthenticatedUserService(authenticatedUserService),
+		rest.WithLogger(logger),
+	)
+
+	adapter := httpadapter.New(server)
+
+	lambda.Start(serverHandler(adapter))
 }
 
 func serverHandler(adapter *httpadapter.HandlerAdapter) func(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
