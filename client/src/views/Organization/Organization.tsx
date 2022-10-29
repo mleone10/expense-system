857c208c2706da264d54e166cb34bfb15786482c
mleone10 <mleone10@gmail.com>
2022-10-29 11:12:04 -0400

    Call GET /orgs/:id on Org page

--- client/src/views/Organization/Organization.tsx
+++ client/src/views/Organization/Organization.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./Organization.css"
 
+interface orgData {
+  name: string
+  id: string
+  creationDate: string
+  members: string[]
+}
+
+const getOrg = async (orgId: string) => {
+  return fetch(`/api/orgs/${orgId}`, {
+    credentials: "include"
+  }).then(response => {
+    if (response.ok) {
+      return response.json().then(body => body as orgData)
+    }
+  })
+}
+
 const Organization = () => {
+  const [orgData, setOrg] = useState<orgData>()
+  const { orgId } = useParams()
+
+  const loadOrg = () => {
+    if (orgId === undefined) {
+      return
+    }
+
+    getOrg(orgId).then(org => {
+      if (org !== undefined) {
+        console.log(org)
+        setOrg(org)
+      }
+    })
+  }
+
+  useEffect(() => {
+    loadOrg()
+  }, [])
+
   return (
     <React.Fragment>
       <h1>Organization</h1>
+      <p>{orgData?.name}</p>
     </React.Fragment>
   )
 }
